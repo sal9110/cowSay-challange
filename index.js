51ecdf3e057c4f980986334662f2d219d49a70ef
Salvo <salvopatti110@gmail.com>
2021-06-14 13:18:45 +0200

    tongue fixed

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const cowSay= require("./node_modules/cowsay");
 console.log(cowSay.say({
     text: `Hello! I'm ${student.name} from the ${student.campus}`,
     e:"^ ^",
-    t:"U",
+    T:"U "
 }))
